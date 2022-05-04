6964eac68711de3a87f2eb50dac2bf45e87c3dbb
akshatsrf <akshatsrf860@gmail.com>
2022-05-04 13:05:54 +0530

    added movies list

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import random
 app = FastAPI()
 
 Recommanded_Games = ["GTA 5","Red Dead Redemption 2","Assassins Creed Valhalla","Valorant","CS:GO","Sleeping Dogs","Need for Speed","Resident Evil"]
+movies = ["Badhai Ho", "Jai ho", "Commando"]
 @app.get("/")
 async def root():
-    choice = random.randint(0,len(Recommanded_Games))
+    # choice = random.randint(0,len(Recommanded_Games))
+    choice = random.randint(0,len(movies))
     #Yaha change kara hai
-    return {"Try this game":Recommanded_Games[choice]}
+    # return {"Try this game":Recommanded_Games[choice]}
+    return {"Try this game":movies[choice]}
